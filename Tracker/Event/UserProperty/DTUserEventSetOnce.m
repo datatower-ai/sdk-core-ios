3b282333fa1855cc87c4d8c0f2d4c65b9e2d9193
shijunxing <shijunxing@nodetower.com>
2022-12-15 10:40:21 +0800

    bugfix：user_set_once 增加事件名

--- Tracker/Event/UserProperty/DTUserEventSetOnce.m
+++ Tracker/Event/UserProperty/DTUserEventSetOnce.m
@@ -12,7 +12,8 @@
 - (instancetype)init {
     if (self = [super init]) {
         self.eventType = DTEventTypeUserSetOnce;
-        
+        self.eventName = DT_EVENT_TYPE_USER_SETONCE;
+
     }
     return self;
 }
