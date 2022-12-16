69f0d0baa6538cf78304bcc8a809305080645492
neo <neo@nodetower.com>
2022-12-16 16:56:29 +0800

    framework

---
+++ DTSDK-0.0.1/build/Pods.build/Release-iphonesimulator/Pods-packager.build/Objects-normal/x86_64/Pods-packager-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /var/folders/88/ypm13q114d78w8sjw9sfg0vw0000gn/T/cocoapods-jczgvkz9/Pods/Target\ Support\ Files/Pods-packager/Pods-packager-dummy.m \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap
