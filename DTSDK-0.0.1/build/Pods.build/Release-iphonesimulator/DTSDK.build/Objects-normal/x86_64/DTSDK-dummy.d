70c4d59b65159b647bd308e0381ca729178019b9
neo <neo@nodetower.com>
2022-12-17 20:59:14 +0800

    delete framework

--- DTSDK-0.0.1/build/Pods.build/Release-iphonesimulator/DTSDK.build/Objects-normal/x86_64/DTSDK-dummy.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /var/folders/88/ypm13q114d78w8sjw9sfg0vw0000gn/T/cocoapods-jczgvkz9/Pods/Target\ Support\ Files/DTSDK/DTSDK-dummy.m \
-  /var/folders/88/ypm13q114d78w8sjw9sfg0vw0000gn/T/cocoapods-jczgvkz9/Pods/Target\ Support\ Files/DTSDK/DTSDK-prefix.pch \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap
