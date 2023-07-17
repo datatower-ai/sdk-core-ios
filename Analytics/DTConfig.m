11988f4bf55e5cbb3e22936af47ba21883fa6d40
Lilin <lilinll77@gmail.com>
2023-07-17 12:29:09 +0800

    iOS重构，tag 2.0.2

--- Analytics/DTConfig.m
+++ Analytics/DTConfig.m
@@ -1,7 +1,7 @@
 #import "DTConfig.h"
 #import "DTBaseEvent.h"
 
-#define DT_IOS_SDK_VERSION_NAME @"2.0.1"
+#define DT_IOS_SDK_VERSION_NAME @"2.0.2"
 #define DT_IOS_SDK_VERSION_TYPE @"iOS"
 static DTConfig * _defaultTDConfig;
 
