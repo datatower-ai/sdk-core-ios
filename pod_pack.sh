96be575adca02b797e48a63bc52b1a86370cb63d
Lilin <lilinll77@gmail.com>
2023-10-10 19:04:35 +0800

    ios sdk，脚本添加module复制逻辑

--- pod_pack.sh
+++ pod_pack.sh
@@ -1,1 +1,5 @@
 pod package DataTowerAICore.podspec --force --no-mangle --exclude-deps --configuration=Release --spec-sources="" -embedded
+
+version=$(find . -name DataTowerAICore-* -type d)
+version=$(echo $version | cut -d "-" -f 2)
+cp -a __Modules/* DataTowerAICore-${version}/ios/DataTowerAICore.framework
