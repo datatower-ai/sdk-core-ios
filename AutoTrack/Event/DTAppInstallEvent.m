c988e5a1ffbbfdfceb2f28134b70ff099d4b3a5d
Lilin <lilinll77@gmail.com>
2024-05-06 14:21:23 +0800

    安装事件属性IS_FOREGROUND放错了地方

--- AutoTrack/Event/DTAppInstallEvent.m
+++ AutoTrack/Event/DTAppInstallEvent.m
@@ -16,7 +16,7 @@
         self.systemUpTime = self.systemUpTime - 0.25;
     }
     NSMutableDictionary *dict = [super jsonObject];
-    dict[COMMON_PROPERTY_IS_FOREGROUND] = @(NO);
+    self.properties[COMMON_PROPERTY_IS_FOREGROUND] = @(NO);
     return dict;
 }
 
